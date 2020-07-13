1f1f1a21cbc4eb6ad48aedcad4fd026a999b74f2
Binis <binisbelev@gmail.com>
2020-07-13 12:10:54 +0300

    Implementation

--- src/main/java/net/binis/demo/spring/BaseEntityModifier.java
+++ src/main/java/net/binis/demo/spring/BaseEntityModifier.java
@@ -1,17 +1,37 @@
 package net.binis.demo.spring;
 
+import com.spiralbank.core.tools.ApplicationContextProvider;
 import lombok.extern.slf4j.Slf4j;
+import net.binis.demo.modifier.Modifier;
+
+import javax.persistence.EntityManager;
+
+import static java.util.Objects.isNull;
 
 @Slf4j
-public class BaseEntityModifier {
+public class BaseEntityModifier implements Modifier {
+
+    private static EntityManager manager;
+
+    private static Object parent;
+
+    private static void init() {
+        if (isNull(manager)) {
+            manager = ApplicationContextProvider.getApplicationContext().getBean(EntityManager.class);
+        }
+    }
 
     public void save() {
-
+        init();
+        manager.persist(parent);
     }
 
     public void delete() {
-
+        manager.remove(parent);
     }
 
-
+    @Override
+    public void setObject(Object parent) {
+        this.parent = parent;
+    }
 }
