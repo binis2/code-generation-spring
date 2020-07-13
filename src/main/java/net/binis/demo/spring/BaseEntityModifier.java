2731fb8be5f6a1e5a3cda2e154fa2473dffda70c
Binis <stanislav.belev@scalefocus.com>
2020-07-13 10:20:45 +0300

    Base initial class.

---
+++ src/main/java/net/binis/demo/spring/BaseEntityModifier.java
@@ -0,0 +1,17 @@
+package net.binis.demo.spring;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class BaseEntityModifier {
+
+    public void save() {
+
+    }
+
+    public void delete() {
+
+    }
+
+
+}
