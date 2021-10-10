315453b4fa3be898091682045c22ab87a9f100e5
Binis <binisbelev@gmail.com>
2021-10-10 13:56:19 +0300

    Added syncSilentExecutor

--- src/main/java/net/binis/codegen/spring/async/executor/CodeExecutor.java
+++ src/main/java/net/binis/codegen/spring/async/executor/CodeExecutor.java
@@ -9,9 +9,9 @@ package net.binis.codegen.spring.async.executor;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -63,6 +63,10 @@ public class CodeExecutor {
     }
 
     public static AsyncExecutor syncExecutor() {
+        return Runnable::run;
+    }
+
+    public static AsyncExecutor syncSilentExecutor() {
         return task -> {
             try {
                 task.run();
@@ -71,5 +75,6 @@ public class CodeExecutor {
             }
         };
     }
+
 
 }
